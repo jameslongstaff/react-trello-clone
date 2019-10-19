b6d2be47151199bbbd9492096750e72d09b98975
James Longstaff <james.longstaff@gmail.com>
2019-10-19 13:49:54 +0100

    Nodemon up and running and fixed issues with startup

--- backend/src/loaders/index.ts
+++ backend/src/loaders/index.ts
@@ -2,12 +2,10 @@ import expressLoader from "./express";
 import dependencyInjectorLoader from "./dependencyInjector";
 import mongooseLoader from "./mongoose";
 import Logger from "./logger";
-//We have to import at least all the events once so they can be triggered
-import "./events";
 
 export default async ({ expressApp }) => {
-  const mongoConnection = await mongooseLoader();
-  Logger.info("✌️ DB loaded and connected!");
+  // const mongoConnection = await mongooseLoader();
+  // Logger.info("✌️ DB loaded and connected!");
 
   /**
    * WTF is going on here?
